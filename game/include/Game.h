ecf8f63ce803ae7105a28932755f7bfc3bf4f926
matiasmyyra <mika.p.koivisto@gmail.com>
2023-12-25 09:21:07 +0200

    Refactor and renane Source.cpp -> GameMain.cpp

--- game/include/Game.h
+++ game/include/Game.h
@@ -1,3 +1,14 @@
 #pragma once
+#include "/OpenGL/common/include/GameWindow.h"
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
